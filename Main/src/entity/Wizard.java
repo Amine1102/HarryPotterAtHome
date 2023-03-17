66a5990983348c6f8d5cb6376810f1a507316b69
Amine1102 <amine.laimeche@gmail.com>
2023-03-17 16:35:28 +0100

    Apres 4h puree enfin

--- Main/src/entity/Wizard.java
+++ Main/src/entity/Wizard.java
@@ -17,24 +17,25 @@ public class Wizard extends Character {
         this.gp = gp;
         this.keyH = keyH;
         setDefaultValues();
+        getPlayerImage();
     }
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
-        direction = "up";
+        direction = "right";
     }
 
     public void getPlayerImage(){
         try{
-            up1 = ImageIO.read(getClass().getResourceAsStream("/main/boy_up_1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/main/boy_up_2.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("/main/boy_down_1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/main/boy_down_2.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/main/boy_right_1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/main/boy_right_2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/main/boy_left_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/main/boy_left_2.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/res/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/res/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/res/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/res/boy_down_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/boy_right_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/res/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/res/boy_left_2.png"));
 
         }catch (IOException e){
             e.printStackTrace();
